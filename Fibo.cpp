dfc57e1ab3aa8694888120e204ad70035035005b
DanilChery <52321592+DanilChery@users.noreply.github.com>
2020-11-27 20:03:25 +0300

    Add files via upload

---
+++ Fibo.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int func(int N) {
+    if (N == 0 ) {
+        return 0;
+    }
+    else if (N == 1) {
+        return 1;
+    }
+    else {
+        return func(N - 1) + func(N - 2);
+    }
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    N = func(N);
+    cout << N;
+
+}
